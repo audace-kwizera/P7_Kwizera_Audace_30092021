8fded14e77fbe904d560bf652081cf5dda67c96a
audace-kwizera <companyfreshbeauty@gmail.com>
2021-10-02 15:49:05 +0200

    les routes principales fonctionnes post profile et user de base pour les get et post

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,3 @@
-const { sequelize } = require(".");
-
 module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define("User", {
         username: {
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -2,5 +2,25 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 
+router.post('/new', (req, res, next) => {
+    db.Post.create({
+        text: req.body.text,
+        UserId: req.body.UserId
+    })
+    .then(newPost => res.send(newPost))
+    .catch();
+});
+
+router.get('/find/:id', (req, res, next) => {
+    db.Post.findAll({
+        wher: { UserId: req.params.id },
+        include: [db.User]
+    })
+    .then(post => res.send(post))
+    .catch();
+});
+
+module.exports = router;
+
 
 module.exports = router;
