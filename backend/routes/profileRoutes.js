f59b269bdea162ca28c608c3fbc6fc5b0f886777
audace-kwizera <companyfreshbeauty@gmail.com>
2021-10-02 14:00:56 +0200

    routes du profil get et post connecté

--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -2,5 +2,22 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 
+router.post('/new', (req, res, next) => {
+    db.Profile.create({
+        name: req.body.name,
+        UserId: req.body.UserId
+    })
+    .then(newProfile => res.send(newProfile))
+    .catch();
+});
+
+router.get('/find/:id', (req, res, next) => {
+    db.Profile.findAll({
+        wher: { UserId: req.params.id },
+        include: [db.User]
+    })
+    .then(profile => res.send(profile))
+    .catch();
+});
 
 module.exports = router;
