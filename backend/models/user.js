f3b38176dc9e26abf8f0d7620ac1cd87e0175b24
audace-kwizera <companyfreshbeauty@gmail.com>
2021-10-02 12:34:37 +0200

    création des models post profile et user

---
+++ backend/models/user.js
@@ -0,0 +1,22 @@
+const { sequelize } = require(".");
+
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define("User", {
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+
+    User.associate = models => {
+        User.hasMany(models.Post, {
+            onDelete: "cascade"
+        });
+
+        User.hasOne(models.Profile, {
+            onDelete: "cascade"
+        })
+    };
+
+    return User;
+};
